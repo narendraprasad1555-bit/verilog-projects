ebcfd60da73adaf9eebac3a81cf29753d9503100
narendraprasad1555-bit <narendraprasad1555@gmail.com>
2026-04-21 12:52:08 +0530

    Add files via upload

---
+++ demux1to16.v
@@ -0,0 +1,43 @@
+//===========================================================================================================
+//DFT Training institute pvt Limited
+//
+//copyrights (C)2024 DFT Training Institute Pvt LTD
+//
+//All rights reserved
+//
+//Project  : designing demux1to16 gate level modelling
+//module   : demux1to4
+//Author   :Narendra prasad.R
+//Date     :03/05/2024
+//Description :
+//
+//version  :v1.1
+//
+//Revision updates
+//0.1-22/05/2024-initial
+//
+//===================================================================================================================
+//module declaration
+module  demux1to16(y0,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15,i,s3,s2,s1,s0);
+//port declaration
+input i,s3,s2,s1,s0;
+output y0,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15;
+//gate instantiation
+assign y0=~s3&~s2&~s1&~s0;
+assign y1=~s3&~s2&~s1&s0;
+assign y2=~s3&~s2&s1&~s0;
+assign y3=~s3&~s2&s1&s0;
+assign y4=~s3&s2&~s1&~s0;
+assign y5=~s3&s2&~s1&s0;
+assign y6=~s3&s2&s1&~s0;
+assign y7=~s3&s2&s1&s0;
+assign y8=s3&~s2&~s1&~s0;
+assign y9=s3&~s2&~s1&s0;
+assign y10=s3&~s2&s1&~s0;
+assign y11=s3&~s2&s1&s0;
+assign y12=s3&s2&~s1&~s0;
+assign y13=s3&s2&~s1&s0;
+assign y14=s3&s2&s1&~s0;
+assign y15=s3&s2&s1&s0;
+//end module declaration
+endmodule
---
+++ tb_demux1to16.v
@@ -0,0 +1,54 @@
+//==================================================================================================================
+//DFT Training institute pvt Limited
+//
+//copyrights (C)2024 DFT Training Institute Pvt LTD
+//
+//All rights reserved
+//
+//Project  : designing demux1to16 gate level modelling
+//module   : demux1to16
+//Author   :Narendra prasad.R
+//Date     :04/05/2024
+//Description :
+//
+//version  :v1.1
+//
+//Revision updates
+//0.1-22/05/2024-initial
+//
+//==================================================================================================================
+//test bench module declaration
+module tb_demux1to16;
+//data type declaraction
+reg  tb_s3,tb_s2,tb_s1,tb_s0,tb_i;
+wire tb_y0,tb_y1,tb_y2,tb_y3,tb_y4,tb_y5,tb_y6,tb_y7,tb_8,tb_y9,tb_y10,tb_y11,tb_y12,tb_y13,tb_y14,tb_y15;
+//instantiation
+demux1to16 o1(.y0(tb_y0),.y1(tb_y1),.y2(tb_y2),.y3(tb_y3),.y4(tb_y4),.y5(tb_y5),.y6(tb_y6),.y7(tb_y7),.y8(tb_y8),.y9(tb_y9),.y10(tb_y10),.y11(tb_y11),.y12(tb_y12),.y13(tb_y13),.y14(tb_y14),.y15(y_15),.i(tb_i),.s3(tb_s3),.s2(tb_s2),.s1(tb_s1),.s0(tb_s0));
+//timing delaration
+initial
+begin
+        tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b0; tb_s1=1'b0; tb_s0=1'b0;
+	#5 tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b0; tb_s1=1'b0; tb_s0=1'b1;	
+	#5 tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b0; tb_s1=1'b1; tb_s0=1'b0;
+        #5 tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b0; tb_s1=1'b1; tb_s0=1'b1;
+	#5 tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b1; tb_s1=1'b0; tb_s0=1'b0;
+	#5 tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b1; tb_s1=1'b0; tb_s0=1'b1;
+        #5 tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b1; tb_s1=1'b1; tb_s0=1'b0;
+        #5 tb_i=1'b1; tb_s3=1'b0; tb_s2=1'b1; tb_s1=1'b1; tb_s0=1'b1;
+        #5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b0; tb_s1=1'b0; tb_s0=1'b0;
+        #5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b0; tb_s1=1'b0; tb_s0=1'b1;
+        #5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b0; tb_s1=1'b1; tb_s0=1'b0;
+        #5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b0; tb_s1=1'b1; tb_s0=1'b1;
+	#5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b1; tb_s1=1'b0; tb_s0=1'b0;
+	#5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b1; tb_s1=1'b0; tb_s0=1'b1;
+	#5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b1; tb_s1=1'b1; tb_s0=1'b0;		
+	#5 tb_i=1'b1; tb_s3=1'b1; tb_s2=1'b1; tb_s1=1'b1; tb_s0=1'b1;
+        #50 $finish;
+end
+//wave form declaration
+initial
+begin
+	$dumpfile("tb_demux1to16.vcd");
+	$dumpvars(0,tb_demux1to16);
+end
+endmodule
